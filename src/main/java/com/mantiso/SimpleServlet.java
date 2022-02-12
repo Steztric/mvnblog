f95bd6abf6a68bab1dafd913be2ad4b6e07aa291
Sterren Fisher <13692795+Steztric@users.noreply.github.com>
2022-02-12 13:58:53 +0800

    Exercise 3.4 - init app config

--- src/main/java/com/mantiso/SimpleServlet.java
+++ src/main/java/com/mantiso/SimpleServlet.java
@@ -1,25 +1,40 @@
 package com.mantiso;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = {"/home", "*.do"})
+@WebServlet(urlPatterns = {"/home", "*.do"}, initParams = {@WebInitParam(name = "ProductName", value = "Welcome Application")})
 public class SimpleServlet extends HttpServlet {
+    String appName;
+
+    @Override
+    public void init() throws ServletException {
+        appName = getInitParameter("ProductName");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        generateResponse(req, resp);
+        String name = req.getParameter("name");
+        if(name != null) {
+            resp.setContentType("text/xml");
+            PrintWriter writer = resp.getWriter();
+            writer.write("<application>");
+            writer.printf("<name>Hello %s!</name>", name);
+            writer.printf("<product>%s</product>", appName);
+            writer.write("</application>");
+        } else {
+            resp.getWriter().write("Please supply a name");
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        generateResponse(req, resp);
-    }
-
-    private void generateResponse(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String name = req.getParameter("name");
         if(name != null) {
             resp.getWriter().printf("Hello %s!", name);
